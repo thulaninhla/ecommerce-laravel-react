fb671e89ce8ea9b26f1eb6a809c73cb140c7d8f0
Thulani Nhlapo <thulaninhlapo2@gmail.com>
2025-06-03 11:31:45 +0200

    Fix Dockerfile and Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -8,8 +9,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-    },
 });
