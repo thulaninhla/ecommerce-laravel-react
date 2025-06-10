4f0124f6e28a798994f0e37a508c163a6a74b489
Thulani Nhlapo <thulaninhlapo2@gmail.com>
2025-06-10 03:36:53 +0200

    Fix HTTPS asset issue for production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // This tells Laravel to generate all URLs using https:// in production.
+        if (env('APP_ENV') !== 'local') {
+        URL::forceScheme('https');
+    }
     }
 }
--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'], // or .jsx if using React
             refresh: true,
+
         }),
-    ],
+    ],  base: '',
 });
